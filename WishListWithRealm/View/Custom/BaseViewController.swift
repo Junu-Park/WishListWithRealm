931c61e09648e8b200600b9b5b15674825230a2f
Park Junwoo <ays99111@naver.com>
2025-03-05 19:41:30 +0900

    [feat] BaseVC 이니셜라이저 구현

--- WishListWithRealm/View/Custom/BaseViewController.swift
+++ WishListWithRealm/View/Custom/BaseViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class BaseViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,4 +28,9 @@ class BaseViewController: UIViewController {
     func configureLayout() { }
     func configureView() { }
     func bind() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
